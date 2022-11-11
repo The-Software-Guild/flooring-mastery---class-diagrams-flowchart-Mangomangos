0ec00f83bdcbc3972a28ad2f19bf580f656f1242
Jenna Wade <jwade6@outlook.com>
2022-11-11 13:37:51 -0500

    added tax class

---
+++ src/main/java/com/jwade/dto/Tax.java
@@ -0,0 +1,44 @@
+package com.jwade.dto;
+
+import java.math.BigDecimal;
+
+public class Tax {
+
+    private String stateAbbreviation;
+
+    private String stateName;
+
+    private BigDecimal taxRate;
+
+    public Tax(){}
+
+    public Tax (String abbreviation, String name, BigDecimal taxRate){
+        this.stateAbbreviation = abbreviation;
+        this.stateName = name;
+        this.taxRate = taxRate;
+    }
+
+    public String getStateAbbreviation() {
+        return stateAbbreviation;
+    }
+
+    public void setStateAbbreviation(String stateAbbreviation) {
+        this.stateAbbreviation = stateAbbreviation;
+    }
+
+    public String getStateName() {
+        return stateName;
+    }
+
+    public void setStateName(String stateName) {
+        this.stateName = stateName;
+    }
+
+    public BigDecimal getTaxRate() {
+        return taxRate;
+    }
+
+    public void setTaxRate(BigDecimal taxRate) {
+        this.taxRate = taxRate;
+    }
+}
