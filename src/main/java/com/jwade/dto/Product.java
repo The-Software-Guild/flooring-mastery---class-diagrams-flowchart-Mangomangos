aa99424911eac20d3340c3cd4853c764f0fefa1b
Jenna Wade <jwade6@outlook.com>
2022-11-11 13:28:51 -0500

    added product class

---
+++ src/main/java/com/jwade/dto/Product.java
@@ -0,0 +1,45 @@
+package com.jwade.dto;
+
+import java.math.BigDecimal;
+
+public class Product {
+
+    private String productType;
+
+    private BigDecimal costPerSquareFoot;
+
+    private BigDecimal laborCostPerSquareFoot;
+
+    public Product (){}
+
+    public Product (String type, BigDecimal cost, BigDecimal labor){
+        this.productType = type;
+        this.costPerSquareFoot = cost;
+        this.laborCostPerSquareFoot = labor;
+    }
+
+
+    public String getProductType() {
+        return productType;
+    }
+
+    public void setProductType(String productType) {
+        this.productType = productType;
+    }
+
+    public BigDecimal getCostPerSquareFoot() {
+        return costPerSquareFoot;
+    }
+
+    public void setCostPerSquareFoot(BigDecimal costPerSquareFoot) {
+        this.costPerSquareFoot = costPerSquareFoot;
+    }
+
+    public BigDecimal getLaborCostPerSquareFoot() {
+        return laborCostPerSquareFoot;
+    }
+
+    public void setLaborCostPerSquareFoot(BigDecimal laborCostPerSquareFoot) {
+        this.laborCostPerSquareFoot = laborCostPerSquareFoot;
+    }
+}
