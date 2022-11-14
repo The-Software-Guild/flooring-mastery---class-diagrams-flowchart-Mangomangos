a77ecf11446b2ed5701af88535ad590fa0f36a97
Jenna Wade <jwade6@outlook.com>
2022-11-14 10:16:31 -0500

    added interface

--- src/main/java/com/jwade/dao/ProductDao.java
+++ src/main/java/com/jwade/dao/ProductDao.java
@@ -1,4 +1,14 @@
 package com.jwade.dao;
 
+import com.jwade.dto.Order;
+import com.jwade.dto.Product;
+
+import java.util.Map;
+
 public interface ProductDao {
+
+    public Order unmarshallOrder(String line);
+
+    public Map<String, Product> readFile(String file);
+
 }
