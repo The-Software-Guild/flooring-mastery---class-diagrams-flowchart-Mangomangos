4c845daa93c9be34b249ba21c87110736b7c35bc
Jenna Wade <jwade6@outlook.com>
2022-11-14 10:47:20 -0500

    created imple of product dao

--- src/main/java/com/jwade/dao/ProductDao.java
+++ src/main/java/com/jwade/dao/ProductDao.java
@@ -1,13 +1,12 @@
 package com.jwade.dao;
 
-import com.jwade.dto.Order;
 import com.jwade.dto.Product;
 
 import java.util.Map;
 
 public interface ProductDao {
 
-    public Order unmarshallOrder(String line);
+    public Product unmarshallProduct(String line);
 
     public Map<String, Product> readFile(String file);
 
