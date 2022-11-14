6e0966e8769555277a4e64f077380c94168a533a
Jenna Wade <jwade6@outlook.com>
2022-11-14 10:47:57 -0500

    created imple of product dao

---
+++ src/main/java/com/jwade/dao/ProductDaoImpl.java
@@ -0,0 +1,58 @@
+package com.jwade.dao;
+
+import com.jwade.dto.Product;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class ProductDaoImpl implements ProductDao{
+
+    private Map<String, Product> products = new HashMap<>();
+
+    private static final String DELIMITER = ",";
+
+    private static final String PRODUCT_FILE = "products.txt";
+    @Override
+    public Product unmarshallProduct(String line) {
+        String[] productTokens = line.split(DELIMITER);
+        String productType = productTokens[0];
+        BigDecimal productCostPerSquareFoot = BigDecimal.valueOf(Double.parseDouble(productTokens[1]));
+        BigDecimal productLaborCostPerSquareFoot = BigDecimal.valueOf(Double.parseDouble(productTokens[2]));
+
+        return new Product(productType, productCostPerSquareFoot, productLaborCostPerSquareFoot);
+
+    }
+
+    @Override
+    public Map<String, Product> readFile(String file) {
+        
+        Scanner scanner = null;
+
+        try {
+            scanner = new Scanner(
+                    new BufferedReader(
+                            new FileReader(PRODUCT_FILE)
+                    )
+            );
+        } catch (FileNotFoundException e){
+        }
+
+        String currentLine;
+        Product currentProduct;
+
+        while (scanner.hasNextLine()) {
+            currentLine = scanner.nextLine();
+            currentProduct = unmarshallProduct(currentLine);
+            products.put(currentProduct.getProductType(), currentProduct);
+        }
+
+        scanner.close();
+        return  products;
+    }
+
+}
