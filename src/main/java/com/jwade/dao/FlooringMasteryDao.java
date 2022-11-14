70662e5060bb66005537497198e129259263f210
Jenna Wade <jwade6@outlook.com>
2022-11-14 18:39:30 -0500

    added methods and implementation for dao file

--- src/main/java/com/jwade/dao/FlooringMasteryDao.java
+++ src/main/java/com/jwade/dao/FlooringMasteryDao.java
@@ -2,19 +2,44 @@ package com.jwade.dao;
 
 import com.jwade.dto.Order;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 public interface FlooringMasteryDao {
 
-    Order getOrder(String order);
+    Order getOrder(Integer orderNumber);
 
     List<Order> listAllOrders();
 
-    Order addOrder(Order order);
+    Order addOrder(String orderDate, Order order);
 
-    Order removeOrder(Order order);
+    Order removeOrder(String orderDate, Order order);
 
-    Order editOrder(Order order);
+    Order editOrder(Order order, String orderDate);
+
+    List<Order> listDayOrders(String orderDate);
+
+    String updateCustomerName(Order order, String newName);
+
+    String updateCustomerState(Order order, String newState);
+
+    BigDecimal updateTaxRate (Order order, BigDecimal newTaxRate);
+
+    String updateProductType(Order order, String newProduct);
+
+    BigDecimal updateArea(Order order, BigDecimal newArea);
+
+    BigDecimal updateCostPerSquareFoot(Order order, BigDecimal newCost);
+
+    BigDecimal updateLaborCostPerSquareFoot(Order order, BigDecimal newLaborCost);
+
+    BigDecimal updateMaterialCost(Order order, BigDecimal newMaterialCost);
+
+    BigDecimal updateLaborCost(Order order, BigDecimal newLaborCost);
+
+    BigDecimal updateTax(Order order, BigDecimal newTax);
+
+    BigDecimal updateTotal(Order order, BigDecimal newTotal);
 
 
 }
--- src/main/java/com/jwade/dao/FlooringMasteryDaoImpl.java
+++ src/main/java/com/jwade/dao/FlooringMasteryDaoImpl.java
@@ -2,31 +2,128 @@ package com.jwade.dao;
 
 import com.jwade.dto.Order;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-public class FlooringMasteryDaoImpl implements FlooringMasteryDao{
+public class FlooringMasteryDaoImpl implements FlooringMasteryDao {
+
+    private Map<Integer, Order> orderMap = new HashMap<>();
+
+    private FileDao fileDao = new FileDaoImpl();
+    private String FILE_PATH;
+
+    public FlooringMasteryDaoImpl() {
+        String FILE_PATH = "Orders/";
+        orderMap = fileDao.readFiles(FILE_PATH);
+
+    }
+
     @Override
-    public Order getOrder(String order) {
-        return null;
+    public Order getOrder(Integer orderNumber) {
+        return orderMap.get(orderNumber);
     }
 
     @Override
     public List<Order> listAllOrders() {
+        return new ArrayList<>(orderMap.values());
+    }
+
+
+    @Override
+    public Order addOrder(String orderDate, Order order) {
+        Order newOrder = orderMap.put(order.getOrderNumber(), order);
+        fileDao.addNewOrderToFile(orderDate, newOrder);
+        return newOrder;
+    }
+
+    @Override
+    public Order removeOrder(String orderDate, Order order) {
         return null;
     }
 
     @Override
-    public Order addOrder(Order order) {
-        return null;
+    public Order editOrder(Order order, String orderDate) {
+        Order updatedOrder = orderMap.put(order.getOrderNumber(), order);
+        fileDao.updateOrderInFile(updatedOrder, orderDate);
+        return updatedOrder;
     }
 
     @Override
-    public Order removeOrder(Order order) {
-        return null;
+    public List<Order> listDayOrders(String orderDate) {
+        Map<Integer, Order> orderDayMap;
+        orderDayMap = fileDao.readFile("Orders/orders_" + orderDate + "txt");
+        return new ArrayList<>(orderDayMap.values());
     }
 
     @Override
-    public Order editOrder(Order order) {
-        return null;
+    public String updateCustomerName(Order order, String newName) {
+        order.setCustomerName(newName);
+        return newName;
     }
+
+    @Override
+    public String updateCustomerState(Order order, String newState) {
+        order.setState(newState);
+        return newState;
+    }
+
+    @Override
+    public BigDecimal updateTaxRate(Order order, BigDecimal newTaxRate) {
+        order.setTax(newTaxRate);
+        return newTaxRate;
+    }
+
+    @Override
+    public String updateProductType(Order order, String newProduct) {
+        order.setProductType(newProduct);
+        return newProduct;
+    }
+
+    @Override
+    public BigDecimal updateArea(Order order, BigDecimal newArea) {
+        order.setArea(newArea);
+        return newArea;
+    }
+
+    @Override
+    public BigDecimal updateCostPerSquareFoot(Order order, BigDecimal newCost) {
+        order.setCostPerSquareFoot(newCost);
+        return newCost;
+    }
+
+    @Override
+    public BigDecimal updateLaborCostPerSquareFoot(Order order, BigDecimal newLaborCost) {
+        order.setLaborCostPerSquareFoot(newLaborCost);
+        return newLaborCost;
+    }
+
+    @Override
+    public BigDecimal updateMaterialCost(Order order, BigDecimal newMaterialCost) {
+        order.setMaterialCost(newMaterialCost);
+        return newMaterialCost;
+    }
+
+    @Override
+    public BigDecimal updateLaborCost(Order order, BigDecimal newLaborCost) {
+        order.setLaborCost(newLaborCost);
+        return newLaborCost;
+    }
+
+    @Override
+    public BigDecimal updateTax(Order order, BigDecimal newTax) {
+        order.setTax(newTax);
+        return newTax;
+    }
+
+    @Override
+    public BigDecimal updateTotal(Order order, BigDecimal newTotal) {
+        order.setTotal(newTotal);
+        return newTotal;
+    }
+
+
 }
+
