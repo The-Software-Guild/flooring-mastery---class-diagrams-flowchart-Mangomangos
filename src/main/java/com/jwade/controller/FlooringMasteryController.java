10617fd245bc7c6a2377ef20c3ee99d73a552aaa
Jenna Wade <jwade6@outlook.com>
2022-11-15 10:58:52 -0500

    added some implementation to edit order and add order methods

--- src/main/java/com/jwade/controller/FlooringMasteryController.java
+++ src/main/java/com/jwade/controller/FlooringMasteryController.java
@@ -2,7 +2,10 @@ package com.jwade.controller;
 
 import com.jwade.dao.FlooringMasteryDaoImpl;
 import com.jwade.dao.FlooringMasteryException;
+import com.jwade.dto.Order;
 import com.jwade.dto.Product;
+import com.jwade.service.FlooringMasteryService;
+import com.jwade.service.FlooringMasteryServiceImpl;
 import com.jwade.ui.FlooringMasteryView;
 
 import java.math.BigDecimal;
@@ -11,11 +14,11 @@ public class FlooringMasteryController {
 
     private FlooringMasteryView view;
 
-    private FlooringMasteryDaoImpl dao;
+    private FlooringMasteryServiceImpl service;
 
-    public FlooringMasteryController(FlooringMasteryView view, FlooringMasteryDaoImpl dao) {
+    public FlooringMasteryController(FlooringMasteryView view, FlooringMasteryServiceImpl service) {
         this.view = view;
-        this.dao = dao;
+        this.service = service;
     }
 
     public void run (){
@@ -34,7 +37,7 @@ public class FlooringMasteryController {
                     editOrder();
                     break;
                 case "4": // remove an order
-                    removeOrder();
+                    removeAnOrder();
                     break;
                 case "5": // export all data
                     exportData();
@@ -52,7 +55,10 @@ public class FlooringMasteryController {
     }
 
     public void addOrder(){
-        String orderDate = view.getOrderDate();
+        while(!service.validateOrderDate(view.getOrderDate())){
+            String orderDate = view.getOrderDate();
+            service.validateOrderDate(orderDate);
+        }
         String orderName = view.getOrderName();
         String orderState = view.getOrderState();
         BigDecimal orderArea = view.getOrderArea();
@@ -65,9 +71,63 @@ public class FlooringMasteryController {
 
     public void editOrder(){
         view.editOrderBanner();
+        String orderDate = view.getOrderDate();
+        Integer orderNumber = view.getOrderNumber();
+        Order currentOrder = service.getOrder(orderDate, orderNumber);
+        boolean keepGoing = true;
+        while (keepGoing) {
+            String operation = view.getMenuSelection();
+            switch (operation) {
+                case "1": // edit customer name
+                    editCustomerName(currentOrder);
+                    break;
+                case "2": // edit state
+                    editState(currentOrder);
+                    break;
+                case "3": // edit product type
+                    editProductType(currentOrder);
+                    break;
+                case "4": // edit area
+                    editArea(currentOrder);
+                case "5": // quit
+                    keepGoing = false;
+                    break;
+                default:
+                    view.displayUnknownCommand();
+            }
+        }
     }
 
-    public void removeOrder(){
+    private void editArea(Order currentOrder) {
+        BigDecimal area = view.getOrderArea();
+        service.validateFloorArea(area);
+        view.confirmationSaveOrder(currentOrder);
+        view.editOrderSuccessful();
+    }
+
+    private void editProductType(Order currentOrder) {
+        view.confirmationSaveOrder(currentOrder);
+        view.editOrderSuccessful();
+    }
+
+    private void editState(Order currentOrder) {
+        String newState = view.getOrderState();
+        service.validateCustomerState(newState);
+        view.confirmationSaveOrder(currentOrder);
+        view.editOrderSuccessful();
+    }
+
+    private void editCustomerName(Order currentOrder) {
+        String newName = view.getOrderName();
+        view.confirmationSaveOrder(currentOrder);
+        view.editOrderSuccessful();
+    }
+
+    public void removeAnOrder(){
+        String orderDate = view.getOrderDate();
+        Integer orderNumber = view.getOrderNumber();
+        Order currentOrder = service.getOrder(orderDate, orderNumber);
+        view.confirmationRemoveOrder(currentOrder);
         view.orderRemovedSuccessful();
     }
 
