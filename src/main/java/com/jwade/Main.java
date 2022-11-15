bb5337e7b1d660c63917ba7193afdc82c1cafc37
Jenna Wade <jwade6@outlook.com>
2022-11-15 11:00:56 -0500

    revised to include service layer

--- src/main/java/com/jwade/Main.java
+++ src/main/java/com/jwade/Main.java
@@ -1,7 +1,21 @@
 package com.jwade;
 
+import com.jwade.controller.FlooringMasteryController;
+import com.jwade.dao.*;
+import com.jwade.service.FlooringMasteryServiceImpl;
+import com.jwade.ui.FlooringMasteryView;
+import com.jwade.ui.UserIO;
+import com.jwade.ui.UserIOImpl;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        UserIO myIO = new UserIOImpl();
+        ProductDaoImpl productDao = new ProductDaoImpl();
+        TaxDaoImpl taxDao = new TaxDaoImpl();
+        FlooringMasteryDaoImpl myDao = new FlooringMasteryDaoImpl();
+        FlooringMasteryView myView = new FlooringMasteryView(myIO);
+        FlooringMasteryServiceImpl myService = new FlooringMasteryServiceImpl(productDao, taxDao, myDao);
+        FlooringMasteryController controller = new FlooringMasteryController(myView, myService);
+        controller.run();
     }
 }
